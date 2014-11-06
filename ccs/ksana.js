84259b35a91fd7eaa1fc1729c22626814e3eba3d
yapcheahshen <yapcheahshen@gmail.com>
2014-11-06 10:51:45 +0800

    for ios

---
+++ ccs/ksana.js
@@ -0,0 +1,34 @@
+jsonp_handler({
+ "version": "1",
+ "build": 73,
+ "title": "中國圖書目錄",
+ "date": "2014-11-06T02:50:55.449Z",
+ "description": "yinshun",
+ "baseurl": "http://littlehateliu.github.io/ccs",
+ "minruntime": "1.2",
+ "files": [
+  "index.html",
+  "build.js",
+  "build.css",
+  "jquery.js",
+  "nodemain.js",
+  "react-with-addons.js",
+  "ksana.js"
+ ],
+ "filesizes": [
+  450,
+  4513940,
+  123163,
+  277980,
+  671712,
+  699
+ ],
+ "filedates": [
+  "2014-11-06T02:37:19.000Z",
+  "2014-11-06T02:36:20.000Z",
+  "2014-11-06T02:36:20.000Z",
+  "2014-08-29T07:08:38.000Z",
+  "2014-08-29T07:08:38.000Z",
+  "2014-11-06T02:50:41.000Z"
+ ]
+})
