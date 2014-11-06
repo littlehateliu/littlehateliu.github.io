ecba66eba5b384c7b99b9206e469c679fdb3ea61
yapcheahshen <yapcheahshen@gmail.com>
2014-11-06 12:39:29 +0800

    fix for android

--- ccs/ksana.js
+++ ccs/ksana.js
@@ -1,11 +1,10 @@
 jsonp_handler({
  "version": "1",
- "build": 73,
+ "build": 74,
  "title": "中國圖書目錄",
- "date": "2014-11-06T02:50:55.449Z",
- "description": "yinshun",
- "baseurl": "http://littlehateliu.github.io/ccs",
- "minruntime": "1.2",
+ "date": "2014-11-06T04:38:52.535Z",
+ "description": "ccs",
+ "baseurl": "http://littlehateliu.github.io/ccs/",
  "files": [
   "index.html",
   "build.js",
@@ -21,14 +20,14 @@ jsonp_handler({
   123163,
   277980,
   671712,
-  699
+  596
  ],
  "filedates": [
   "2014-11-06T02:37:19.000Z",
-  "2014-11-06T02:36:20.000Z",
-  "2014-11-06T02:36:20.000Z",
+  "2014-11-06T02:50:55.000Z",
+  "2014-11-06T02:50:55.000Z",
   "2014-08-29T07:08:38.000Z",
   "2014-08-29T07:08:38.000Z",
-  "2014-11-06T02:50:41.000Z"
+  "2014-11-06T04:38:36.000Z"
  ]
 })
